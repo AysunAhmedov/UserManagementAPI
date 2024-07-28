0b31d62308848ff860b6db90a092880c4e3dd28b
Icehack <icehack@abv.bg>
2024-07-28 19:50:54 +0300

    commit

--- src/main/java/com/crud/app/usercrud/dto/UserDTO.java
+++ src/main/java/com/crud/app/usercrud/dto/UserDTO.java
@@ -10,14 +10,14 @@ public class UserDTO {
     @Schema(hidden = true)
     private Long id;
 
-    @Pattern(regexp="^[A-Za-z]*$",message = "First name should contain only letters.")
     @NotBlank(message = "Please provide a First Name.")
+    @Pattern(regexp="^[A-Za-z]*$",message = "First name should contain only letters.")
     @Size(min = 2,max = 50 , message = "First name should be between 2 and 50 letters.")
     @Schema(description = "The first name of the user.", example = "Aysun", minLength = 2, maxLength = 50)
     private String firstName;
 
-    @Pattern(regexp="^[A-Za-z]*$",message = "Last name should contain only letters.")
     @NotBlank(message = "Please provide a Last Name.")
+    @Pattern(regexp="^[A-Za-z]*$",message = "Last name should contain only letters.")
     @Size(min = 2,max = 50 , message = "Last name should be between 2 and 50 letters.")
     @Schema(description = "The last name of the user.", example = "Ahmedov", minLength = 2, maxLength = 50)
     private String lastName;
@@ -27,14 +27,14 @@ public class UserDTO {
     @Schema(description = "The date of birth of the user.", example = "1994-05-30")
     private LocalDate dateOfBirth;
 
+    @NotBlank(message = "Please provide a phone number.")
     @Pattern(regexp="^\\d+$",message = "Phone number should contain only digits.")
     @Size(min = 10,max = 10, message = "Phone number must be exactly 10 digits.")
     @Schema(description = "The phone number of the user.", example = "0895916607", minLength = 10, maxLength = 10)
     private String phoneNumber;
 
-
-    @Schema(description = "The e-mail of the user", example = "aysun@gmail.com")
     @NotBlank(message = "Please provide an E-mail.")
+    @Schema(description = "The e-mail of the user", example = "aysun@gmail.com")
     @Email(message = "Please provide a valid email address.")
     private String emailAddress;
 
